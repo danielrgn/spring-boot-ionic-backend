0263b0648b75cc6b35243626b1ebd9b376da8b73
Daniel Rigoni <danielrgn@hotmail.com>
2019-01-07 14:42:41 -0200

    Incluindo uma nova categoria com POST

--- src/main/java/com/danielrgn/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/danielrgn/cursomc/resources/CategoriaResource.java
@@ -1,14 +1,19 @@
 package com.danielrgn.cursomc.resources;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.servlet.Servlet;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.danielrgn.cursomc.domain.Categoria;
 import com.danielrgn.cursomc.services.CategoriaService;
@@ -24,5 +29,13 @@ public class CategoriaResource {
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = categoriaService.buscar(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = categoriaService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
