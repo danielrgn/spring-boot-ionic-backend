cb6d8bdd7babbcd6bfe611417878cb1ac108f6dd
Daniel Rigoni <daniel.rigoni@luizalabs.com>
2019-03-04 15:12:30 -0300

    Implementando autorizacao

--- src/main/java/com/danielrgn/cursomc/config/SecurityConfig.java
+++ src/main/java/com/danielrgn/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.danielrgn.cursomc.security.JWTAuthenticationFilter;
+import com.danielrgn.cursomc.security.JWTAuthorizationFilter;
 import com.danielrgn.cursomc.security.JWTUtil;
 
 @Configuration
@@ -56,6 +57,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.antMatchers(PUBLIC_MACHERS).permitAll()
 		.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
